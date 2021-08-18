bbff12079180c4ffe2bce671510fb4c6ba4496be
Ivan Tolmachev <ivatolm@gmail.com>
2021-08-18 14:46:57 +0300

    Create JoinRoom and Ready api functions

--- src/api.py
+++ src/api.py
@@ -17,6 +17,36 @@ def create_room():
 
     if tools.validate(data, { "name": str, "players_count": int }):
       server.create_room(data["name"], data["players_count"])
+    else:
+      raise Exception("Data validation failed.")
 
   except Exception as e:
     API_LOGGER.log("API :: CreateRoom", str(e))
+
+
+@app.route("/api/joinRoom", methods=["POST"])
+def join_room():
+  try:
+    data = request.json()
+
+    if tools.validate(data, { "room_id": str, "nickname": str }):
+      server.join_room(data["room_id"], data["nickname"])
+    else:
+      raise Exception("Data validation failed.")
+
+  except Exception as e:
+    API_LOGGER.log("API :: JoinRoom", str(e))
+
+
+@app.route("/api/ready", methods=["POST"])
+def ready():
+  try:
+    data = reqeust.json()
+
+    if tools.validate(data, { "player_id": str, "ready": bool }):
+      server.ready(data["player_id"], data["ready"])
+    else:
+      raise Exception("Data validation failed.")
+
+  except Exception as e:
+    API_LOGGER.log("API :: Ready", str(e))
