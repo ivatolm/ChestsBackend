8a83c9340070cd953586eb7005d2a92ccd9923e1
Ivan Tolmachev <ivatolm@gmail.com>
2021-09-11 23:13:50 +0300

    Remove player states. Improve room state

--- src/room.py
+++ src/room.py
@@ -29,71 +29,28 @@ class Room:
     ):
       self.players[player_id] = {
         "nickname": nickname,
-        "ready": False,
-        "turn": False,
         "cards": [],
-        "state": 0
+        "turn": False,
+        "wait": False
       }
 
-      if len(self.players) == self.settings["players_count"]:
-        self.st = 1
-        for player_id in self.players.keys():
-          self.players[player_id]["state"] = 1
+      self.players[player_id]["wait"] = True
+      self.__try_ch_st(1)
 
       return player_id, self.settings
 
     raise Exception("Unique id generation failed.")
 
 
-  @exception_logger(fail_output=False)
-  def set_ready(self, player_id):
-    if self.st != 1:
-      raise Exception("This action isn't allowed in current game state.")
-
-    if player_id in self.players:
-      if self.players[player_id]["state"] != 2:
-        raise Exception("This action isn't allowed in current player state.")
-
-      self.players[player_id]["state"] = 3
-      self.players[player_id]["ready"] = True
-      return True
-
-    raise Exception("Player with given id wasn't found.")
-
- 
-  @exception_logger(fail_output=False)
-  def wait(self, player_id):
-    if self.st != 1:
-      raise Exception("This action isn't allowed in current game state.")
-
-    if player_id in self.players:
-      if self.players[player_id]["state"] != 3:
-        raise Exception("This action isn't allowed in current player state.")
-
-      while (
-        (len(self.players) != self.settings["players_count"])
-          or
-        (False in [player["ready"] for player in self.players.values()])
-      ):
-        time.sleep(1)
-      self.players[player_id]["state"] = 1
-      self.players[player_id]["ready"] = False
-
-      return True
-
-    raise Exception("Player with given id wasn't found.")
-
-
   @exception_logger(fail_output=(-1, []))
   def state(self, player_id):
-    if self.st != 1:
-      raise Exception("This action isn't allowed in current game state.")
+    if not self.__wait_st(1):
+      raise Exception("Failed to block on state-change waiting.")
 
     if player_id in self.players:
-      if self.players[player_id]["state"] != 1:
-        raise Exception("This action isn't allowed in current player state.")
+      self.players[player_id]["wait"] = True
+      self.__try_ch_st(2)
 
-      self.players[player_id]["state"] = 2
       return (
         self.players[player_id]["turn"],
         list(self.players[player_id]["cards"]),
@@ -104,17 +61,14 @@ class Room:
 
   @exception_logger(fail_output=False)
   def take(self, player_id, nickname, card):
-    if self.st != 1:
-      raise Exception("This action isn't allowed in current game state.")
+    if not self.__wait_st(2):
+      raise Exception("Failed to block on state-change waiting.")
 
     if (
       (player_id in self.players)
         and
       (nickname in [player["nickname"] for player in self.players.values()])
     ):
-      if self.players[player_id]["state"] != 2:
-        raise Exception("This action isn't allowed in current player state.")
-
       target_player = None
       for player_id, player_data in self.players.items():
         if player_data["nickname"] == nickname:
@@ -133,16 +87,55 @@ class Room:
 
   @exception_logger(fail_output=False)
   def pull(self, player_id):
-    if self.st != 1:
-      raise Exception("This action isn't allowed in current game state.")
+    if not self.__wait_st(2):
+      raise Exception("Failed to block on state-change waiting.")
 
     if player_id in self.players:
-      if self.players[player_id]["state"] != 2:
-        raise Exception("This action isn't allowed in current player state.")
-
       card = random.choice(self.deck)
       self.players[player_id]["cards"].append(card)
       self.deck.remove(card)
       return True
 
     raise Exception("Player with given id wasn't found.")
+
+
+  @exception_logger(fail_output=False)
+  def set_ready(self, player_id):
+    if not self.__wait_st(2):
+      raise Exception("Failed to block on state-change waiting.")
+
+    if player_id in self.players:
+      self.players[player_id]["wait"] = True
+      self.__try_ch_st(1)
+
+      return True
+
+    raise Exception("Player with given id wasn't found.")
+
+
+  @exception_logger(fail_output=False)
+  def __wait_st(self, state):
+    if not (
+      (self.st == 0 and state == 1) or
+      (self.st == 1 and state == 2) or
+      (self.st == 2 and state == 1)
+    ):
+      return False
+
+    while self.st != state:
+      time.sleep(1)
+
+    return True
+
+
+  @exception_logger(fail_output=False)
+  def __try_ch_st(self, new_state):
+    change_cond = sum([player["wait"] for player in self.players])
+
+    if change_cond == self.settings["players_count"]:
+      self.st = new_state
+      for player_id, _ in self.players.items():
+        self.players[player_id]["wait"] = False
+      return True
+
+    return False
