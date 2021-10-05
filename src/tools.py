66d2128df1684296631101126601613a2a66ea21
Ivan Tolmachev <ivatolm@gmail.com>
2021-10-05 10:59:25 +0300

    Fix 'tools/validate' function

--- src/tools.py
+++ src/tools.py
@@ -2,28 +2,28 @@ from . import logger
 
 
 logger = logger.Logger("tools")
+exception_logger = logger.gen_exception_logger()
 
 
+@exception_logger(fail_output=False)
 def validate(x, y):
-  try:
-    if not y:
-      raise Exception("Sample object is None.")
+  if not y:
+    raise Exception("Sample object is None.")
 
+  results = {}
+  for key, item in x.items():
     result = True
-    for key, item in y.items():
-      if key not in x:
-        result = False
+    if key not in y or not isinstance(item, y[key]):
+      result = False
+    results[key] = result
+    print(f"{key} ({type(item).__name__}) :: " + ("pass" if result else "fail"))
 
-      if not isinstance(x[key], item):
-        result = False
+  for key, item in y.items():
+    result = True
+    if key not in x:
+      result = False
+    results[key] = result
+    print(f"{key}: ({type(item).__name__}) :: " +
+      ("pass" if result else "fail"))
 
-      logger.log(
-        "validate",
-        f"{key} ({type(x[key]).__name__} <> {item.__name__}) :: " +
-          ("pass" if result else "fail"))
-
-    return result
-
-  except Exception as e:
-    logger.log("validate", str(e))
-    return False
+  return bool(sum([item for item in results.values()]))
